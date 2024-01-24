ec24e8fece213512e80cd0bbd9f0d538e963e460
Suzi Curran <suzicurran@gmail.com>
2024-01-24 15:45:15 -0500

    with test watcher

--- src/kanaNode.test.ts
+++ src/kanaNode.test.ts
@@ -1,5 +1,4 @@
 import KanaNode from "./kanaNode";
-import {describe, it, expect} from '@jest/globals';
 
 describe("KanaNode", () => {
     it('stores the provided initial value', () => {
@@ -10,7 +9,7 @@ describe("KanaNode", () => {
         expect(kanaNode.value).toBe('は');
     })
 
-    it('is initialized with no children', () => {
+    it('is initialized with an empty array of children', () => {
         // act
         const kanaNode = new KanaNode('の');
 
@@ -18,15 +17,61 @@ describe("KanaNode", () => {
         expect(kanaNode.children.length).toBe(0);
     })
 
-    it('can add a provided node to its children', () => {
-        // arrange
-        const parentNode = new KanaNode('は');
-        const nodeToAdd = new KanaNode('か');
+    describe("addChild", () => {
+        it('can add a provided node to its children', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            const nodeToAdd = new KanaNode('か');
 
-        // act
-        parentNode.addChild(nodeToAdd);
+            // act
+            parentNode.addChild(nodeToAdd);
 
-        // assert
-        expect(parentNode.children[0]).toBe(nodeToAdd);
+            // assert
+            expect(parentNode.children[0]).toBe(nodeToAdd);
+        })
+
+        it('does not check for duplicates before adding a child node', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            const kaChildToAdd = new KanaNode('か');
+            const saChildToAdd = new KanaNode('さ');
+
+            // act
+            parentNode.addChild(kaChildToAdd);
+            parentNode.addChild(saChildToAdd);
+
+            // assert
+            expect(parentNode.children.length).toBe(2);
+        })
+    })
+
+    describe("getChildOrNullByValue", () => {
+        it('returns a child whose value is an exact match for provided string', () => {
+                // arrange
+                const parentNode = new KanaNode('は');
+                const nodeToAdd = new KanaNode('や');
+                parentNode.addChild(nodeToAdd);
+    
+                // act
+                const result = parentNode.getChildOrNullByValue('や');
+    
+                // assert
+                expect(result).toBe(nodeToAdd);
+        })
+
+        it('returns null if no child with exact value exists', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            parentNode.children.push(
+                new KanaNode('の'), 
+                new KanaNode('な')
+            );
+
+            // act
+            const result = parentNode.getChildOrNullByValue('お');
+
+            // assert
+            expect(result).toBe(null);
+    })
     })
 })
