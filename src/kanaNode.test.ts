9fe946681b2a961302e4bef73ef0a38609594f9e
Suzi Curran <suzicurran@gmail.com>
2024-01-24 21:21:40 -0500

    now with merge functionality

--- src/kanaNode.test.ts
+++ src/kanaNode.test.ts
@@ -1,5 +1,4 @@
 import KanaNode from "./kanaNode";
-import {describe, it, expect} from '@jest/globals';
 
 describe("KanaNode", () => {
     it('stores the provided initial value', () => {
@@ -10,7 +9,7 @@ describe("KanaNode", () => {
         expect(kanaNode.value).toBe('は');
     })
 
-    it('is initialized with no children', () => {
+    it('is initialized with an empty array of children', () => {
         // act
         const kanaNode = new KanaNode('の');
 
@@ -18,15 +17,104 @@ describe("KanaNode", () => {
         expect(kanaNode.children.length).toBe(0);
     })
 
-    it('can add a provided node to its children', () => {
-        // arrange
-        const parentNode = new KanaNode('は');
-        const nodeToAdd = new KanaNode('か');
+    describe("addChild", () => {
+        it('can add a provided node to an empty array of children', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            const nodeToAdd = new KanaNode('か');
 
-        // act
-        parentNode.addChild(nodeToAdd);
+            // act
+            parentNode.addChild(nodeToAdd);
 
-        // assert
-        expect(parentNode.children[0]).toBe(nodeToAdd);
+            // assert
+            expect(parentNode.children[0]).toBe(nodeToAdd);
+        })
+
+        it('does not add a duplicate child node', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            const kaChildToAdd = new KanaNode('か');
+
+            // act
+            parentNode.addChild(kaChildToAdd);
+            parentNode.addChild(kaChildToAdd);
+
+            // assert
+            expect(parentNode.children.length).toBe(1);
+        })
+
+        it('merges the children of the provided node into an existing match', () => {
+            // arrange
+            const parentNode = new KanaNode('ぶ');
+            parentNode.addChild(new KanaNode('は'));
+            const childToAdd = new KanaNode('は');
+            childToAdd.addChild(new KanaNode('そ'));
+
+            // act
+            parentNode.addChild(childToAdd);
+
+            // assert
+            expect(parentNode.children.length).toBe(1);
+            const haChild = parentNode.children[0];
+            expect(haChild).not.toBeNull;
+            expect(haChild!.children.length).toBe(1);
+        })
+
+        it('applies the isTerminal property when merging in a smaller word', () => {
+            // arrange
+            const iNode = new KanaNode('い');
+            const naNode = new KanaNode('な');
+            const kaNode = new KanaNode('か');
+            naNode.addChild(kaNode);
+            iNode.addChild(naNode);
+
+            const childToAdd = new KanaNode('な');
+            const terminalNode = new KanaNode('か');
+            terminalNode.isTerminal = true;
+            childToAdd.addChild(terminalNode);
+
+            // act
+            iNode.addChild(childToAdd);
+
+            // assert
+            expect(iNode.children.length).toBe(1);
+            const middleNode = iNode.children[0]
+            expect(middleNode).not.toBeNull;
+            expect(middleNode.children.length).toBe(1);
+            const finalNode = middleNode.children[0];
+            expect(finalNode.value).toBe('か');
+            expect(finalNode.children.length).toBe(0);
+            expect(finalNode.isTerminal).toBe(true);
+        })
+    })
+
+    describe("getChildOrNullByValue", () => {
+        it('returns a child whose value is an exact match for provided string', () => {
+                // arrange
+                const parentNode = new KanaNode('は');
+                const nodeToAdd = new KanaNode('や');
+                parentNode.children.push(nodeToAdd);
+    
+                // act
+                const result = parentNode.getChildOrNullByValue('や');
+    
+                // assert
+                expect(result).toBe(nodeToAdd);
+        })
+
+        it('returns null if no child with exact value exists', () => {
+            // arrange
+            const parentNode = new KanaNode('は');
+            parentNode.children.push(
+                new KanaNode('の'), 
+                new KanaNode('な')
+            );
+
+            // act
+            const result = parentNode.getChildOrNullByValue('お');
+
+            // assert
+            expect(result).toBe(null);
+    })
     })
 })
--- src/kanaNode.ts
+++ src/kanaNode.ts
@@ -9,11 +9,28 @@ class KanaNode {
         this.isTerminal = false;
     }
 
-    getChildOrNullByValue(value: string) {
+    // retrieves a child node by value, if one exists
+    getChildOrNullByValue(value: string) : KanaNode | null {
         return this.children.find(child => child.value === value) ?? null;
     }
 
+    // takes a KanaNode and adds it to the array of children, merging with existing nodes of same value as necessary
     addChild(newChild: KanaNode) {
+        const existingMatch = this.getChildOrNullByValue(newChild.value);
+
+        // if the value already exists, append the child nodes by this same method
+        if (existingMatch && newChild.children.length > 0) {
+            newChild.children.forEach(child => {existingMatch.addChild(child)});
+            return;
+        }
+
+        // if it exists but this was the last node to append, ensure isTerminal is false
+        if (existingMatch) {
+            existingMatch.isTerminal = true;
+            return;
+        }
+
+        // else, we can add the supplied node as-is
         this.children.push(newChild);
     }
 }
